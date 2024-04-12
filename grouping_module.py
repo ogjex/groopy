27db2e368183f0460b3c287ea7a316149e70a7f3
ogjex <81006912+ogjex@users.noreply.github.com>
2024-04-12 10:25:10 +0200

    change university to education and add unittest

--- grouping_module.py
+++ grouping_module.py
@@ -19,11 +19,11 @@ class GroupingModule:
             gender_groups[person.gender].append(person)
         return gender_groups
     
-    def group_people_by_university(people, num_groups):
+    def group_people_by_education(people, num_groups):
         # Create a dictionary to store people based on their university
         university_groups = defaultdict(list)
         for person in people:
-            university_groups[person.university].append(person)
+            university_groups[person.education].append(person)
 
         # Calculate the number of people per group
         num_people_per_group = len(people) // num_groups
@@ -41,64 +41,3 @@ class GroupingModule:
                 groups[i % num_groups].append(person)
 
         return groups
-
-    def sort_people_by_undesirables(self, people):
-        # Sort people based on the number of undesirables
-        sorted_people = sorted(people, key=lambda x: len(x.undesirables), reverse=True)
-
-        # Ensure that sorting does not exceed the maximum number of groups
-        max_groups_needed = max(len(people) // len(set(len(person.undesirables) for person in people)), 1)
-        sorted_people = sorted_people[:max_groups_needed * self.max_total_groups]
-
-        return sorted_people
-
-    def sort_people(self, people, gender, university):
-        # Sort people based on undesirables first
-        sorted_people = sorted(people, key=lambda x: len(x.undesirables), reverse=True)
-        
-        # Sort by mutual desirables
-        sorted_people = sorted(sorted_people, key=lambda x: (len(set(x.desirables) & set([p.name for p in people]))), reverse=True)
-        
-        # Sort by different universities
-        sorted_people = sorted(sorted_people, key=lambda x: x.university)
-        
-        # Sort by gender
-        sorted_people = sorted(sorted_people, key=lambda x: x.gender)
-        
-        return sorted_people
-
-    def group_by_university(self, people):
-        university_groups = defaultdict(list)
-        for person in people:
-            university_groups[person.university].append(person)
-        return university_groups
-
-    def assign_groups(self):
-        gender_groups = self.group_by_gender()
-        for gender, people_group in gender_groups.items():
-            people_group = self.sort_people(people_group, gender, university=None)
-            university_groups = self.group_by_university(people_group)
-            for university, university_group in university_groups.items():
-                university_group = self.sort_people(university_group, gender, university)
-                self.assign_groups_for_university(university_group, gender, university)
-                if len(self.groups) >= self.max_total_groups:
-                    break
-            if len(self.groups) >= self.max_total_groups:
-                break
-        return self.groups
-
-    def assign_groups_for_university(self, university_group, gender, university):
-        groups_for_person = defaultdict(list)
-        for person in university_group:
-            if len(groups_for_person[person.name]) < self.max_groups_per_person:
-                for group in self.groups:
-                    if len(group.members) < self.max_group_size and person not in group.members:
-                        if all(undesirable not in group.members for undesirable in person.undesirables):
-                            group.add_member(person)
-                            groups_for_person[person.name].append(group)
-                            break
-                if len(groups_for_person[person.name]) >= self.max_groups_per_person:
-                    break
-                if len(self.groups) >= self.max_total_groups:
-                    break
-        return self.groups
--- person.py
+++ person.py
@@ -1,12 +1,12 @@
 class Person:
-    def __init__(self, name, gender, experience, career_preference, desirables, undesirables, university):
+    def __init__(self, name, gender, experience, career_preference, desirables, undesirables, education):
         self.name = name
         self.gender = gender
         self.experience = experience
         self.career_preference = career_preference
         self.desirables = desirables
         self.undesirables = undesirables
-        self.university = university
+        self.education = education
 
     def __str__(self):
-        return f"Name: {self.name}, Gender: {self.gender}, University: {self.university}, Experience: {self.experience}, Career Preference: {self.career_preference}"
+        return f"Name: {self.name}, Gender: {self.gender}, Education: {self.education}, Experience: {self.experience}, Career Preference: {self.career_preference}"
