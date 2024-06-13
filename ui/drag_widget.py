720e22b2d5eca74a2fd297b7869178e4dde6f14f
ogjex <m.agnu.ha@gmail.com>
2024-06-13 11:08:12 +0200

    add abstract class for dragwidgets to inherit from and implement dragcheckbox

--- ui/drag_widget.py
+++ ui/drag_widget.py
@@ -1,21 +1,10 @@
-from PyQt6.QtCore import QMimeData, Qt, pyqtSignal
+from PyQt6.QtCore import QMimeData, Qt
 from PyQt6.QtGui import QDrag, QPixmap
-from PyQt6.QtWidgets import QLabel
+from PyQt6.QtWidgets import QWidget, QLabel, QCheckBox
 
-class DragLabel(QLabel):
-    def __init__(self, person_id: int, person_name: str, parent=None):
-        super().__init__(str(person_name), parent)  # Ensure text is a string
-        self.person_id = person_id
-        self.person_name = person_name
-        # The size needs refactoring, so a drag label creates a target indicator that can be used for itself
-        self.setContentsMargins(25, 5, 25, 5)
-        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.setStyleSheet("border: 1px solid black;")
-        # Store data separately from display label, but use label for default.
-        self.data = (self.person_id, self.person_name)
-
-    def set_data(self, data):
-        self.data = data
+class DragWidget(QWidget):
+    def __init__(self, parent: QWidget | None = ..., flags: Qt.WindowType = ...) -> None:
+        super().__init__(parent, flags)
 
     def mouseMoveEvent(self, e):
         if e.buttons() == Qt.MouseButton.LeftButton:
@@ -31,6 +20,29 @@ class DragLabel(QLabel):
 
             drag.exec(Qt.DropAction.MoveAction)
             self.show() # Show this widget again, if it's dropped outside.
+
+class DragLabel(QLabel, DragWidget):
+    def __init__(self, person_id: int, person_name: str, parent=None):
+        super().__init__(str(person_name), parent)  # Ensure text is a string
+        self.person_id = person_id
+        self.person_name = person_name
+        # The size needs refactoring, so a drag label creates a target indicator that can be used for itself
+        self.setContentsMargins(25, 5, 25, 5)
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setStyleSheet("border: 1px solid black;")
+        # Store data separately from display label, but use label for default.
+        self.data = (self.person_id, self.person_name)
+
+    
+    def set_data(self, data):
+        self.data = data
+
+class DragCheckbox(QCheckBox, DragWidget):
+    def __init__(self, value: str, parent=None):
+        QCheckBox.__init__(self, value, parent)
+        DragWidget.__init__(self, parent)
+        self.value = value
+        self.setContentsMargins(25, 5, 25, 5)
 
 class DragTargetIndicator(QLabel):
     def __init__(self, parent=None):
